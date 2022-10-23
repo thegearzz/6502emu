4282ff7074b0e76e150acb80b72e7744a7d8f0d0
thegearzz <thegearzz@gmail.com>
2022-10-23 18:35:54 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function parseOpCode(opCode){
+	switch(opCode){
+		case ADC:
+			//add memory to accumulator and carry
+			break:
+		case AND:
+			//AND memort to accumulator
+			break:
+		case ASL:
+			//shift left one bit (memory or accumulator)
+			break:
+		case BCC:
+			//branch on carry clear
+			break:
+		case.BCS:
+			//branch on carry set
+			break:
+		case BEQ:
+			//branch if result zero
+		}
+	}
+}
